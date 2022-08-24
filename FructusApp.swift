0f9de2236b07e188217eff3b9d5bff757aebe2c9
bimo.ez <bimo.ez@kakaomobility.com>
2022-08-24 18:17:01 +0900

    기본 리소스 파일 추가

---
+++ FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by bimo.ez on 2022/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
